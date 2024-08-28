8a6619d712af0b1d7f68a9d16c26505682931f81
Steven11vm <stevenvilla10@gmail.com>
2024-08-28 12:04:37 -0500

    opium88888s

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
 
 # Crear la instancia de la aplicación Flask
-app = Flask(__name__)
 app = Flask(__name__, static_folder='static')
 
 # Definir la ruta para la página principal
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+# Definir la ruta para la página "Sobre la IA"
+@app.route('/IAgotic')
+def sobre_la_ia():
+    return render_template('IAgotic.html')
+
+@app.route('/')
+def home():
     return render_template('index.html')
 
 # Ejecutar la aplicación si este archivo es el principal
